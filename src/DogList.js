064c8611b5451a57226b94049c584c1dfb792406
Chandni <chandni1141996@gmail.com>
2021-05-05 13:46:03 +0530

    displaying list

--- src/DogList.js
+++ src/DogList.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
+import './DogList.css';
 
 class DogList extends Component {
     render() {
+
         return (
-            <h1>DOG LIST!</h1>
+            
+            <div className = "DogList">
+                <h1 className = "display-1 text-center">DOG LIST!</h1>
+
+                <div className = "container">
+                    <div className = "row">
+
+                        {this.props.dogs.map(d => (
+                            <div className="Dog col-lg-4 text-center" key={d.name}>
+                                <img src={d.src} alt={d.name} />
+                                <h3>{d.name}</h3>
+                            </div>
+                        ))}
+
+                    </div>
+                </div>
+            </div>
+
         )
     }
 }

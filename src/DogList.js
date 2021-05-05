5a7ee7f720ae93d91d9440601d3c101764e4e863
Chandni <chandni1141996@gmail.com>
2021-05-05 13:12:53 +0530

    starter records

---
+++ src/DogList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class DogList extends Component {
+    render() {
+        return (
+            <h1>DOG LIST!</h1>
+        )
+    }
+}
+
+export default DogList;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
+
 import './index.css';
+
+import "bootstrap"; //JS file
+import "bootstrap/dist/css/bootstrap.min.css"; //CSS file
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ <BrowserRouter>
+      <App />
+ </BrowserRouter>,
   document.getElementById('root')
 );
 
